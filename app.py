8d6693ee082714e339b48b23b059856b9d6f9c15
chetumore <141807044+chetumore@users.noreply.github.com>
2024-06-25 14:10:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,141 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from sklearn.linear_model._base import _preprocess_data
+
+# Load the Random Forest CLassifier model
+filename = 'score-lr-model.pkl'
+regressor = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/FirstInningPrediction', methods=['POST'])
+def FirstInningPrediction():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+        
+        
+        return render_template('FirstInningResult.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+	
+@app.route('/SecondInningPrediction', methods=['POST'])	
+def SecondInningPrediction():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+        target = int(request.form['target'])
+
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+        
+        img_src_batting = batting_team + ".png"
+        img_src_bowling = bowling_team + ".png"
+
+        if target > my_prediction:
+            img_src = img_src_bowling
+        elif target < my_prediction:
+            img_src = img_src_batting
+
+        return render_template('SecondInningResult.html', img_src = img_src)
+
+if __name__ == '__main__':
+    app.run(debug=True)
